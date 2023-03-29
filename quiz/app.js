a1affc1462cc94e155434821d08468aad032be84
Sruthi Laya <sruthipla9@gmail.com>
2023-03-29 12:19:31 +0530

    quiz app

---
+++ quiz/app.js
@@ -0,0 +1,88 @@
+const quizData = [{
+    question: "Q1: 2+3=?",
+    a: "4",
+    b: "5",
+    c: "3",
+    d: "6",
+    correct: "b",
+},
+{
+    question: "Q2: 2-3=?",
+    a: "1",
+    b: "0",
+    c: "-1",
+    d: "-5",
+    correct: "c",
+},
+{
+    question: "Q3: 3*5=?",
+    a: "16",
+    b: "10",
+    c: "15",
+    d: "20",
+    correct: "c",
+}
+
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Hii, you've scored ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
